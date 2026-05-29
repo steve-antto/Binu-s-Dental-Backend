642d08758e2e7841cb04589c380a6f71fbfed14a
Steve Antto D <stevethaliyath22@gmail.com>
2026-05-30 02:17:33 +0530

    Solved Web

--- routes/auth.js
+++ routes/auth.js
@@ -104,4 +104,35 @@ authRouter.patch('/users/:id/language', authenticate, async (req, res) => {
     }
 });
 
+// GET CURRENT USER
+authRouter.get(
+  "/me",
+  authenticate,
+  async (req, res) => {
+    try {
+      const user = await User.findById(
+        req.user._id
+      ).select("-password");
+
+      if (!user) {
+        return res.status(404).json({
+          message: "User not found",
+        });
+      }
+
+      res.json({
+        success: true,
+        user,
+      });
+    } catch (error) {
+      console.error(error);
+
+      res.status(500).json({
+        message:
+          "Failed to fetch user",
+      });
+    }
+  }
+);
+
 export default authRouter;
